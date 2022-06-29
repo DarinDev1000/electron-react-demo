988d37b408c37e997fd5a46d5c43ab79810d1783
Darin <darindev1000@gmail.com>
2022-06-29 00:20:15 -0400

    finish tutorial - working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import ReactWeather, { useOpenWeather } from 'react-open-weather';
 import './App.css';
 
+
 function App() {
+  const { data, isLoading, errorMessage } = useOpenWeather({
+    key: '309479adfcb6fcd4141c4d45202d4120',
+    lat: '1.2921',
+    lon: '36.8219',
+    lang: 'en',
+    unit: 'metric', // values are (metric, standard, imperial)
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ReactWeather
+        isLoading={isLoading}
+        errorMessage={errorMessage}
+        data={data}
+        lang="en"
+        locationLabel="Nairobi"
+        unitsLabels={{ temperature: 'C', windSpeed: 'Km/h' }}
+        showForecast
+      />
     </div>
   );
 }
